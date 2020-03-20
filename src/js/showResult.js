09e15ce5f52e4127070497824106b4ec0cfe9acc
Nils <nils.a687@gmail.com>
2020-03-20 10:28:38 +0100

    first working resultTable

--- src/js/showResult.js
+++ src/js/showResult.js
@@ -1,4 +1,27 @@
 export default function showResult(resultPackage, setResult){
-    console.log(resultPackage)
-    setResult(resultPackage[0])
+    
+    for (let i = 0;i < resultPackage.length; i++){
+        const arrDate = new Date(resultPackage[i].arrival.timestamp*1000)
+        const depDate = new Date(resultPackage[i].departure.timestamp*1000)
+
+
+        const getTime = (a) => {
+            const min = a.getMinutes() < 10 ? "0"+a.getMinutes() : a.getMinutes()
+            return(a.getHours()+':'+min)
+        }
+        const getFullDate = (a) => (
+            a.getDate()+'.'+a.getMonth()+'.'+a.getFullYear()
+        )
+
+        resultPackage[i].arrival.date = getFullDate(arrDate)
+        resultPackage[i].arrival.time = getTime(arrDate)
+
+        resultPackage[i].departure.date = getFullDate(depDate)
+        resultPackage[i].departure.time = getTime(depDate)
+
+        //duration: {hour: 14, minutes: 35}
+        const durMin = resultPackage[i].duration.minutes < 10 ? "0"+resultPackage[i].duration.minutes : resultPackage[i].duration.minutes
+        resultPackage[i].dur = resultPackage[i].duration.hour+':'+durMin 
+    }
+    setResult(resultPackage)
 }
