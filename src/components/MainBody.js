9c59a02c9016b3f0b98b2ebfd87ba499f91d2288
Nils <nils.a687@gmail.com>
2020-06-05 17:24:09 +0200

    typeahead

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -9,7 +9,7 @@ import DisplayPart from './DisplayPart'
 import ShowResults from './ShowResult'
 
 export class MyBody extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleInputChange = this.handleInputChange.bind(this)
@@ -17,19 +17,16 @@ export class MyBody extends Component {
         this.state = firstMainState //in js Folder
     }
 
-   async handleSubmit(e) {
+    async handleSubmit(e) {
         e.preventDefault()
         const mes = this.state.textarea
-        if(!mes){return}
+        if (!mes) { return }
         const updateState = async () => {
-            
             this.setState((old) => {
                 const newState = old
-
-                newState.messanges.push({text: mes, key : newState.messanges.length, user: true})
+                newState.messanges.push({ text: mes, key: newState.messanges.length, user: true })
                 newState.textarea = ""
                 newState.toServer.message = mes
-
                 return newState
             })
         }
@@ -37,82 +34,87 @@ export class MyBody extends Component {
 
         //Anfrage
         const url = 'http://localhost:8080'
-        try{
-            const response = await fetch(url ,{
+        try {
+            const response = await fetch(url, {
                 method: "POST",
                 body: JSON.stringify(this.state.toServer),
-                headers:{
-                    'Content-Type':'application/json'
+                headers: {
+                    'Content-Type': 'application/json'
                 }
             })
             //Antwort
             const json = await response.json()
             //Verarbeitung
-            const[informationPackage, answerPackege, resultPackage] = processResponse(json)
-            showMessages(answerPackege, (a) => {this.setState(a)})
+            const [informationPackage, answerPackege] = processResponse(json)
+            showMessages(answerPackege, (a) => { this.setState(a) })
             //showResult(resultPackage, (a) => {this.setState(old => Object.assign({},old,{results: a}))})
-            this.setState((old)=>{
+            this.setState((old) => {
                 const newState = old
-                newState.toServer.informationPackage = Object.assign(old.toServer.informationPackage,informationPackage)
+                newState.toServer.informationPackage = Object.assign(old.toServer.informationPackage, informationPackage)
                 newState.emotion = answerPackege.emotion
                 return newState
             })
 
         }
-        catch(error){
+        catch (error) {
 
         }
     }
 
 
     handleInputChange(event) {
+        console.log(event)
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
-    
+
         this.setState({
-          [name]: value
+            [name]: value
         });
-      }
-    
-    async handleResultButton(event){
+    }
+
+    async handleResultButton(event) {
         //Result anfrage
-        if(this.state.displayResult === false){
-         const url = 'http://localhost:8080/request'
-          try{
-              const response = await fetch(url ,{
-                  method: "POST",
-                  body: JSON.stringify(this.state.toServer),
-                  headers:{
-                      'Content-Type':'application/json'
-                  }
-             })
-             const json = await response.json()
-             showResult(json, (a) => {this.setState(old => Object.assign({},old,{results: a}))})
-          }
-         catch{
-  
-          }
+        if (this.state.displayResult === false) {
+            const url = 'http://localhost:8080/request'
+            try {
+                const response = await fetch(url, {
+                    method: "POST",
+                    body: JSON.stringify(this.state.toServer),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                const json = await response.json()
+                showResult(json, (a) => { this.setState(old => Object.assign({}, old, { results: a })) })
+            }
+            catch{
+
+            }
         }
-        this.setState(old => (Object.assign({},old, {displayResult: !old.displayResult})))
+        this.setState(old => (Object.assign({}, old, { displayResult: !old.displayResult })))
     }
 
     render() {
         return (
             <Fragment>
-                <MyEmotionPic emotion={this.state.emotion}/>
-                <div style={{overflow: "hidden"}}>
-                    <div style={{width:this.state.displayResult?"50%":"100%", float: 'left'}}>
-                        <DisplayPart 
-                            messanges={this.state.messanges} 
+                <MyEmotionPic emotion={this.state.emotion} />
+                <div style={{ overflow: "hidden" }}>
+                    <div style={{ width: this.state.displayResult ? "50%" : "100%", float: 'left' }}>
+                        <DisplayPart
+                            messanges={this.state.messanges}
                             infos={this.state.toServer.informationPackage}
-                            changeDisplayResult = {this.handleResultButton}
+                            changeDisplayResult={this.handleResultButton}
                         />
-                        <MyUserinput handleSubmit={this.handleSubmit} handleInputChange={this.handleInputChange} textarea={this.state.textarea}/>
+                        <MyUserinput
+                            handleSubmit={this.handleSubmit}
+                            handleInputChange={this.handleInputChange}
+                            textarea={this.state.textarea}
+                            searchStation={false} />
                     </div>
-                    {this.state.displayResult?<div style={{float: "left", width: "50%"}}>
-                        <ShowResults results={this.state.results}/>
-                    </div>:null}
+                    {this.state.displayResult ? <div style={{ float: "left", width: "50%" }}>
+                        <ShowResults results={this.state.results} />
+                    </div> : null}
                 </div>
             </Fragment>
         )
--- src/components/MyUserinput.js
+++ src/components/MyUserinput.js
@@ -1,24 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+//import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 
 export default function MyUserinput(props) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [options, setOptions] = useState([]);
+
+    const handleSearch = (name) => {
+        setIsLoading(true);
+
+        fetch('http://localhost:8080/autofill', {
+            method: "POST",
+            body: JSON.stringify({ name }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then(res => {
+                setOptions(res);
+                setIsLoading(false);
+            })
+    }
+
+
     return (
-        <Form onSubmit={props.handleSubmit}> 
-        <Form.Group className='text-left'>
-            <Form.Label> Eingabe  </Form.Label>
-            <Form.Control 
-                onKeyDown={(event) => {if (event.keyCode === 13) { props.handleSubmit(event); return false}}}
-                onChange={props.handleInputChange}
-                name="textarea"
-                as="textarea" 
-                rows="3" 
-                value={props.textarea}/>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-            Senden
+        <Form onSubmit={props.handleSubmit}>
+            <Form.Group className='text-left'>
+                <Form.Label> Eingabe  </Form.Label>
+                {props.searchStation ?
+                    <AsyncTypeahead
+                        name="textarea"
+                        id="async-example"
+                        isLoading={isLoading}
+                        labelKey="login"
+                        minLength={3}
+                        onSearch={handleSearch}
+                        options={options}
+                        placeholder="Suche nach Station"
+                        dropup={true}
+                        onChange={(e) => {
+                            props.handleInputChange(
+                                {
+                                    target: { name: "textarea", type: "typeahead", value: e[0] },
+                                }
+                            )
+                        }}
+                        value={props.textarea}
+                    />
+                    :
+                    <Form.Control
+                        onKeyDown={(event) => { if (event.keyCode === 13) { props.handleSubmit(event); return false } }}
+                        onChange={props.handleInputChange}
+                        name="textarea"
+                        as="textarea"
+                        rows="3"
+                        value={props.textarea} />}
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Senden
         </Button>
-    </Form>
+        </Form>
     )
+
 }
