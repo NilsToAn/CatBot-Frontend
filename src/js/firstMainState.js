4a6e79c987432b833f652432a802e56a6be4365c
Nils <nils.a687@gmail.com>
2020-07-08 23:24:02 +0200

    listdate in firstState

--- src/js/firstMainState.js
+++ src/js/firstMainState.js
@@ -12,7 +12,7 @@ const firstMainState = {
         {
           origin: ['Bochum','Dortmund'], // can be multiple citys, extra field for auto-complet via js-req
           destination: ['Hannover'], // see origin
-          date: [5, 8, 2020], // only one date, can be changed via interace and after request-done
+          date: [[5, 8, 2020]], // only one date, can be changed via interace and after request-done
           time: [], // see date
           traveller: 1,
           budget: 0,
