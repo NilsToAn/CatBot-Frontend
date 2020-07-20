7d2acf289bfd77705d8152477fb744a8d17fc76c
Nils <nils.a687@gmail.com>
2020-07-20 17:59:17 +0200

    better resultlist

--- src/components/ShowOneResult.js
+++ src/components/ShowOneResult.js
@@ -2,39 +2,24 @@ import React from 'react'
 import '../css/oneResultTable.css'
 
 export default function ShowOneResult(props) {
-    console.log(props)
     const r = props.result
     return (
-        <div>
-            <table className = "oneResultTable">
-                <thead>
-                    <tr>
-                        <th>Anbieter</th>
-                        <th>Tag</th>
-                        <th>Ab/An</th>
-                        <th></th>
-                        <th>Dauer/Umstiege</th>
-                        <th>Gesamtpreis</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{r.provider}</td>
-                        <td>{r.arrival.date}</td>
-                        <td>{r.departure.time}</td>
-                        <td>{r.origin}</td>
-                        <td>{r.dur}</td>
-                        <td>{r.price}</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td>{r.arrival.time}</td>
-                        <td>{r.destination}</td>
-                        <td>{r.transfers}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+       <>
+            <tr>
+                <td>{r.provider}</td>
+                <td>{r.arrival.date}</td>
+                <td>{r.departure.time}</td>
+                <td>{r.origin}</td>
+                <td>{r.dur}</td>
+                <td>{r.price}</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>{r.arrival.time}</td>
+                <td>{r.destination}</td>
+                <td>{r.transfers}</td>
+            </tr>
+        </>
     )
 }
--- src/components/ShowResult.js
+++ src/components/ShowResult.js
@@ -1,19 +1,71 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ShowOneResult from './ShowOneResult'
+import { orderBy } from "lodash";
 
-export default function ShowResult(props) {
-    const MyResultComponents = []
-    for(let i = 0; i < props.results.length; i++){
-        if(props.results[i].arrival){
-            MyResultComponents.push(
-                <ShowOneResult result={props.results[i]} key={props.results[i].departure.timestamp}/>        
-                )
-        }
+
+export default class ShowResult extends Component{
+    state = {
+        collection: [],
+        sortParams: {direction: undefined}
     }
+    componentDidUpdate(){
+       if(this.props.results.length && this.props.results.length !== this.state.collection.length){
+           this.setState({collection: this.props.results})
+       }
+    }
+
+    handleColumnHeaderClick(sortKey) {
+        const {collection,sortParams: { direction }} = this.state;
+
+        // Check, what direction now should be
+        const sortDirection = direction === "desc" ? "asc" : "desc";
+
+        // Sort collection  
+        const sortedCollection = orderBy(
+          collection,
+          ["sortKey"],
+          [sortDirection]
+        );
+
+        //Update component state with new data
+        this.setState({
+          collection: sortedCollection,
+          sortParams: {
+            direction: sortDirection
+          }
+        })
+      }
+
+    render(){
+        let MyResultComponents = []
+        if(this.state.collection){
+            const {collection} = this.state
+            MyResultComponents = (collection.filter(o => o.arrival)).map(o => (
+                <ShowOneResult result={o} key={o.departure.timestamp}/>
+            ))
+        }
+    
+    
+
     return (
-        <div>
+        <div className = "TabelContainer">
+            <table className = "oneResultTable">
+                <thead>
+                    <tr>
+                        <th onClick={() => this.handleColumnHeaderClick("provider")}>Anbieter</th>
+                        <th onClick={() => this.handleColumnHeaderClick("arrival")}>Tag</th>
+                        <th onClick={() => this.handleColumnHeaderClick("provider")}>Ab/An</th>
+                        <th></th>
+                        <th onClick={() => this.handleColumnHeaderClick("dur")}>Dauer/Umstiege</th>
+                        <th onClick={() => this.handleColumnHeaderClick("price")}>Gesamtpreis</th>
+                    </tr>
+                </thead>
+                <tbody>
             {MyResultComponents}
+            </tbody>
+            </table>
         </div>
     )
+}
 }
 
