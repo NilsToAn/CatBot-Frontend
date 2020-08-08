dac080fe93abb5f2305e483cec09cd77bce88e47
Nils <nils.a687@gmail.com>
2020-08-08 19:35:45 +0200

    links are hyper

--- src/components/OneMessage.js
+++ src/components/OneMessage.js
@@ -3,12 +3,33 @@ import PropTypes from 'prop-types'
 
 export default function OneMessage (props) {
   //console.log(props)
-  const { text, user} = props.data
+  let link = ""
+  let replacement = ""
+  let { text, user} = props.data
+  const linkParts1 = text.split('}')
+  if (linkParts1.length === 2) {
+      const linkparts2 = linkParts1[0].split('{')
+      if( linkparts2.length === 2){
+         link = linkparts2[1]
+         const replPart1 = linkParts1[1].split(']')
+         if(replPart1.length === 1){
+           text = linkparts2[0]
+           replacement = replPart1[0].substring(1)
+         }else if(replPart1.length === 2){
+            text = linkparts2[0]
+            replacement = replPart1[0].substring(1)
+         }
+      }
+  }
+
+
+
   return (
     <div className={`message ${user ? 'User' : 'Bot'}`}>
       <div className="message__text">
         <div className="message__text__content">
-          {text} 
+          {text}
+          {link ? <a href={link} target="_blank" rel="noopener noreferrer">{replacement}</a>:null}
         </div>
       </div>
     </div>
